2b474d284d554da51a4498ff0c2d458a3464987c
temis <54607597+tofetpuzo@users.noreply.github.com>
2024-07-25 19:15:51 +0100

    update http-error.filter.ts

--- src/common/http-error.filter.ts
+++ src/common/http-error.filter.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
+// Catch errors
 @Catch(HttpException)
 export class HttpErrorFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
